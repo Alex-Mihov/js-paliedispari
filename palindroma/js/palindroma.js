5b45c9a0c24ac909a3b90150e7e73c81613036a2
Alex Mihov <alexmihov.as@gmail.com>
2025-01-08 14:49:29 +0100

    ragionamento logico

---
+++ palindroma/js/palindroma.js
@@ -0,0 +1,10 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+// chiediamo all'utente la parola 
+
+// FUNZIONE
+
+// verifischiamo se la parola è palindroma 
