f019d782f772a74983891d960a93dab93a88eaca
Alex Mihov <alexmihov.as@gmail.com>
2025-01-08 17:05:26 +0100

    feat vincitore

--- pari-e-dispari/js/pari-e-dispari.js
+++ pari-e-dispari/js/pari-e-dispari.js
@@ -14,10 +14,10 @@ const numeroUser = parseInt(prompt("inserisci un numero da 1 a 5"));
 console.log("hai scelto il numero:", numeroUser);
 
 // generiamo un numero random (sempre da 1 a 5) per il computer
-function numeroComputerRandom() {
+function numeroRandomComputer() {
     return Math.floor(Math.random() * 5) + 1;
 }
-const numeroComputer = numeroComputerRandom();
+const numeroComputer = numeroRandomComputer();
 console.log("il computer ha scelto il numero:", numeroComputer);
 
 // facciamo la somma dei due numeri
@@ -26,5 +26,26 @@ let somma = numeroUser + numeroComputer;
 console.log("la somma dei due numeri è:", somma);
 
 // verifichiamo se il numero è paro o dispari 
+function vincitore() {
+
+    if (somma % 2 === 0) {
+        return 'pari';
+    } 
+    else {
+        return 'dispari';
+    }
+
+}
+const numeroFinale = vincitore()
+console.log("il numero è", numeroFinale)
 
 // dichiariamo il vincitore 
+if (pariODispari === numeroFinale && somma % 2 === 0) {
+    console.log("hai vinto il numero è pari")
+}
+else if (pariODispari === numeroFinale && !somma % 2 === 0) {
+    console.log("hai vinto il numero è dispari")
+}
+else {
+    console.log("spiacente hai perso")
+}
