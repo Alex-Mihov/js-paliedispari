fceed10f98d268da58a23a58ec11e9b07102a9a1
Alex Mihov <alexmihov.as@gmail.com>
2025-01-08 15:42:31 +0100

    pari e dispari human code

---
+++ pari-e-dispari/js/pari-e-dispari.js
@@ -0,0 +1,17 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+
+// chiediamo all'user scegliere se pari o dispari e di inserire un numero da 1 a 5
+
+// generiamo un numero random (sempre da 1 a 5) per il computer
+
+// facciamo la somma dei due numeri
+
+// verifichiamo se il numero è paro o dispari 
+
+// dichiariamo il vincitore 
