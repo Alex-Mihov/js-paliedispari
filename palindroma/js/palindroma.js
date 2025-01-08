c642c353cba362390330a1399898ec0e42cf83ea
Alex Mihov <alexmihov.as@gmail.com>
2025-01-08 14:53:15 +0100

    fix human code

--- palindroma/js/palindroma.js
+++ palindroma/js/palindroma.js
@@ -4,7 +4,13 @@
 
 
 // chiediamo all'utente la parola 
+let parola = prompt("inserisci una parola");
 
 // FUNZIONE
 
 // verifischiamo se la parola è palindroma 
+
+// SE la parola è palindroma 
+    // allora output (la parola è palindroma)
+
+// ALTRIMENTI la parola non è plindroma 
