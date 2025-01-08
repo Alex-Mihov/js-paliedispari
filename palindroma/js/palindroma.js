c49e4c2949c88d6797669a819440246ff0c69cbe
Alex Mihov <alexmihov.as@gmail.com>
2025-01-08 15:24:38 +0100

    feat esercizio palindromo

--- palindroma/js/palindroma.js
+++ palindroma/js/palindroma.js
@@ -6,11 +6,27 @@
 // chiediamo all'utente la parola 
 let parola = prompt("inserisci una parola");
 
-// FUNZIONE
+// FUNZIONE per verificare se la parola è plindroma
+
+function parolaPalindroma(parola) {
+    // converto la parola in minuscolo
+    let parolaMinuscolo = parola.toLowerCase();
+
+    // inverto parola dell'user
+    let parolaInvertita = parolaMinuscolo.split("").reverse("").join("");
+
+    // stampo la parola in console
+    return parolaMinuscolo === parolaInvertita
+}
 
 // verifischiamo se la parola è palindroma 
-
 // SE la parola è palindroma 
+if (parolaPalindroma(parola)) {
     // allora output (la parola è palindroma)
+    console.log(`La parola che ha inserito:"${parola}" è palindroma!`)
+}// ALTRIMENTI la parola non è plindroma 
+else {
+    console.log(`La parola che ha inserito:"${parola}" non è palindroma!`)
+}
 
-// ALTRIMENTI la parola non è plindroma 
+
